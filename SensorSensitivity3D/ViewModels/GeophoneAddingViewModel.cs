ef8b21a2e67402220d019b7a92b210a22647dc16
Milman <alex-sdt@yandex.ru>
2020-02-17 09:40:17 +1000

    few changes

---
+++ SensorSensitivity3D/ViewModels/GeophoneAddingViewModel.cs
@@ -0,0 +1,18 @@
+﻿using SensorSensitivity3D.Domain.Entities;
+using SensorSensitivity3D.ViewModels.Base;
+using System.Collections.Generic;
+
+namespace SensorSensitivity3D.ViewModels
+{
+    public class GeophoneAddingViewModel : BaseViewModel
+    {
+        public int GeophoneCount { get; set; }
+        public List<Geophone> Geophones { get; set; }
+
+
+        protected override void OnDispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
